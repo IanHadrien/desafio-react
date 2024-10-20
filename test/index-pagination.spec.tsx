45b7cb2648707c5bb92e0e5a634a5c0671fe9b78
Ian Hadrien <ianhadrien@gmail.com>
2024-10-20 19:52:33 -0300

    fix: fix test index pagination

--- test/index-pagination.spec.tsx
+++ test/index-pagination.spec.tsx
@@ -11,11 +11,18 @@ const mockContextValue = {
     { id: '2', name: 'Item 2', description: 'Description 2', priority: 'baixa', created_at: '02/01/2023' },
     { id: '3', name: 'Item 3', description: 'Description 3', priority: 'baixa', created_at: '03/01/2023' },
     { id: '4', name: 'Item 4', description: 'Description 4', priority: 'baixa', created_at: '04/01/2023' },
+    { id: '5', name: 'Item 5', description: 'Description 5', priority: 'baixa', created_at: '05/01/2023' },
+    { id: '6', name: 'Item 6', description: 'Description 6', priority: 'baixa', created_at: '06/01/2023' },
+    { id: '7', name: 'Item 7', description: 'Description 7', priority: 'baixa', created_at: '07/01/2023' },
+    { id: '8', name: 'Item 8', description: 'Description 8', priority: 'baixa', created_at: '08/01/2023' },
+    { id: '9', name: 'Item 9', description: 'Description 9', priority: 'baixa', created_at: '09/01/2023' },
+    { id: '10', name: 'Item 10', description: 'Description 10', priority: 'baixa', created_at: '10/01/2023' },
+    { id: '11', name: 'Item 11', description: 'Description 11', priority: 'baixa', created_at: '10/01/2023' },
   ],
   searchItemsByName: vi.fn(),
   clearSearchItems: vi.fn(),
   getPaginatedItems: vi.fn().mockReturnValue([]),
-  totalPages: 2,            // Total pages
+  totalPages: 10,            // Total pages
   saveItemsContext: vi.fn(),
   updateItemsContext: vi.fn(),
   deleteItemContext: vi.fn(),
@@ -26,34 +33,27 @@ const mockContextValue = {
 
 describe('Index page - pagination', () => {
   test('Should return correctly paginated items to the current page', () => {
+
     mockContextValue.getPaginatedItems.mockImplementation((page, itemsPerPage) => {
-      const startIndex = (page - 1) * itemsPerPage
-      const endIndex = startIndex + itemsPerPage
-      return mockContextValue.itens.slice(startIndex, endIndex)
-    })
+      const startIndex = (page - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      return mockContextValue.itens.slice(startIndex, endIndex);
+    });
 
     const { getByText, queryByText, getByTestId } = render(
       <ItensContext.Provider value={mockContextValue}>
         <Index />
       </ItensContext.Provider>
-    )
+    );
 
-    expect(getByText('Item 1')).toBeInTheDocument()
-    expect(getByText('Item 2')).toBeInTheDocument()
+    expect(getByText('Item 1')).toBeInTheDocument();
+    expect(getByText('Item 2')).toBeInTheDocument();
 
-    expect(queryByText('Item 3')).not.toBeInTheDocument()
-    expect(queryByText('Item 4')).not.toBeInTheDocument()
+    expect(queryByText('Item 11')).not.toBeInTheDocument();
 
-    const nextPageButton = getByTestId('next-page')
-    fireEvent.click(nextPageButton)
+    const nextPageButton = getByTestId('next-page');
+    fireEvent.click(nextPageButton);
 
-    expect(getByText('Item 3')).toBeInTheDocument()
-    expect(getByText('Item 4')).toBeInTheDocument()
-
-    expect(queryByText('Item 1')).not.toBeInTheDocument()
-    expect(queryByText('Item 2')).not.toBeInTheDocument()
-
-    expect(mockContextValue.getPaginatedItems).toHaveBeenCalledWith(1, 2)
-    expect(mockContextValue.getPaginatedItems).toHaveBeenCalledWith(2, 2)
-  })
+    expect(getByText('Item 11')).toBeInTheDocument();
+  });
 })
